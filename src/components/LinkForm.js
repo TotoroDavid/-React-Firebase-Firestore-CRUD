22bd0f59cde98bc069aa67f7565355cc83c24878
TotoroDavid <davidtoro65@hotmail.com>
2021-04-23 10:49:38 -0500

    waiting for to fix the connection problems

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const LinkForm = () => {
+const LinkForm = (props) => {
+
+    const initialStateValues = {
+        url : '',
+        name : '',
+        description : ''
+    }
+
+    const [values, setValues] = useState(initialStateValues)
+
+    const handleInputChange = e => {
+        const {name, value} = e.target
+        setValues({...values,[name]:value})        
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addOrEditLink(values)
+    }
+
     return (
-        <form className='card card-body'>
+        <form className='card card-body' onSubmit={handleSubmit}>
             <div className='form-group input-group'>
                 <div className="input-group-text bg-light">
                     <i className="material-icons">insert_link</i>
@@ -12,6 +31,7 @@ const LinkForm = () => {
                     className='form-control'
                     placeholder='https://example.com'
                     name='url'
+                    onChange={handleInputChange}
                 />
             </div>
             <div className='form-group input-group'>
@@ -23,8 +43,22 @@ const LinkForm = () => {
                     className='form-control'
                     name='name'
                     placeholder='webSiteName'
+                    onChange={handleInputChange}
                 />
             </div>
+            <div className='form-group'>
+                <textarea 
+                name="" 
+                rows="3"
+                className='form-control'
+                placeholder='write a description'
+                onChange={handleInputChange}
+                >
+                </textarea>
+            </div>
+            <button className='btn btn-primary btn-block'>
+                save
+            </button>
         </form>
     )
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import LinkForm from './LinkForm'
+import { db } from '../firebase'
 
 const Links = () => {
+
+    const addOrEditLink = async (linkObject) => {
+        await db.collection("links").doc().set(linkObject);
+        console.log('new task added')
+    }
+
     return (
         <div>
-            <h1>links</h1>
+            <LinkForm  addOrEditLink={addOrEditLink} />
+            <h1>Links</h1>
         </div>
     )
 }
